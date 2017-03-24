ff8666c5fdf656b4190b18886c7159ac273c7081
rattandeep1998 <merattandeep@gmail.com>
2017-03-25 00:25:35 +0530

    controller action functionality and views added

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -8,14 +8,39 @@ class AuthenticationController < ApplicationController
 	end
 
 	def signin
+		email = params[:email]
+		password = params[:password]
+		user = User.find_by_email(email)
 
+		if user
+			if user.password == password
+				session[:user_id] = user.id
+				return redirect_to '/'
+			else
+				return redirect_to '/signin'		
+			end
+		else
+			return redirect_to '/signup'
+		end
 	end
 
 	def signup
+		email = params[:email]
+		password = params[:password]
+		user = User.find_by_email(email)
 
+		unless user
+			user = User.create(email: email,password: password)
+			session[:user_id] = user.id
+			return redirect_to '/'
+		else
+			return redirect_to '/signup'
+		end		
 	end
 
 	def logout
+    	session[:user_id] = nil
+    	return redirect_to '/'
+  	end
 
-	end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
-	def index
 
-	end
+	def index
+  		if user_signed_in?
+  			@user = current_user
+  		else
+  			return redirect_to '/signin'
+  		end
+  	end
+
 end
