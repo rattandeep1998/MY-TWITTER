bc71f9f74bb2fabdddbd6d1bd17b55b9a2a9914f
rattandeep1998 <merattandeep@gmail.com>
2017-03-25 22:21:51 +0530

    index and create_tweet actions for home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,13 @@ class HomeController < ApplicationController
 	before_action :authenticate_user
 
 	def index
+		@tweets = Tweet.includes(:user).all.order(created_at: :desc).limit(50)
   end
 
   def create_tweet
+  	content = params[:content]
+  	current_user.tweets.create(content: content)
+  	return redirect_to '/'
   end
 
   def like
@@ -12,5 +16,5 @@ class HomeController < ApplicationController
 
   def follow
   end
-  
+
 end
